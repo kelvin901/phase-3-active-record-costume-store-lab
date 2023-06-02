bbb35633bcf636679e43222676278ad186b27c15
kelvin901 <wawerukelvin2019@gmail.com>
2023-06-02 08:01:41 +0300

    Completed Assignment

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,13 @@
-# Create your Costume class here
-# It should inherit from ActiveRecord::Base
+class CreateCostumeStores < ActiveRecord::Migration
+    def change
+      create_table :costume_stores do |t|
+        t.string :name
+        t.string :location
+        t.integer :costume_inventory
+        t.integer :num_of_employees
+        t.boolean :is_in_business
+        t.datetime :opening_time
+        t.datetime :closing_time
+      end
+    end
+  end
--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,11 @@
-# Create your CostumeStore class here
+class CreateCostumes < ActiveRecord::Migration
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
